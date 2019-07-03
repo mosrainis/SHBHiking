c4094317f76dd41d375afb078319c64bb7551c42
mosrainis <mostafaes5520@gmail.com>
2019-07-03 16:05:57 +0430

    Redesigned template using Vue

---
+++ src/components/sidenav.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="mobile-nav" class="m-nav-container" v-bind:class="[mobile_nav ? 'm-nav-show' : 'm-nav-hide']">
+        <div class="navs">
+            <router-link to="/">خانه</router-link>
+            <router-link to="/history">برنامه‌های گذشته</router-link>
+            <router-link to="/schedule">برنامه‌های پیش‌رو</router-link>
+            <router-link to="/about">درباره ما</router-link>
+            <router-link to="/contact">تماس با ما</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'sidenav',
+    props: [
+        "mobile_nav"
+    ]
+}
+</script>
+
---
+++ src/views/About.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>goodbye</h1>
+</template>
